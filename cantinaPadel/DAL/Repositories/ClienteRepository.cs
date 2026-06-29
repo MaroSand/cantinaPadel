a540534fba40f06d97bc03c1c4c047c6ec235de5
Facumko <facumirchenko@gmail.com>
2026-06-29 18:44:44 -0300

    ii

---
+++ cantinaPadel/DAL/Repositories/ClienteRepository.cs
@@ -0,0 +1,51 @@
+﻿using cantinaPadel.Models;
+using Microsoft.EntityFrameworkCore;
+namespace cantinaPadel.DAL.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        public List<Cliente> ObtenerTodos()
+        {
+
+            using var context = new AppDbContext();
+            return context.Clientes
+                .Include(c => c.Persona)
+                .Where(c => c.Persona.Activo)
+                .ToList();
+        }
+        public Cliente? ObtenerPorId(int id)
+        {
+            using var context = new AppDbContext();
+            return context.Clientes
+                .Include(c => c.Persona)
+                .FirstOrDefault(c => c.IdCliente == id && c.Persona.Activo);
+        }
+        public void Agregar(Cliente cliente)
+        {
+            using var context = new AppDbContext();
+            context.Clientes.Add(cliente);
+            context.SaveChanges();
+        }
+        public void Modificar(Cliente cliente)
+        {
+            using var context = new AppDbContext();
+            context.Clientes.Update(cliente);
+            context.SaveChanges();
+        }
+        public void Bajalogica(int id)
+        {
+            using var context = new AppDbContext();
+            var cliente = context.Clientes.Find(id);
+            if (cliente != null)
+            {
+                cliente.Persona.Activo = false;
+                context.SaveChanges();
+            }
+        }
+        public Persona? BuscarPersonaPorDni(string dni)
+        {
+            using var context = new AppDbContext();
+            return context.Personas.FirstOrDefault(p => p.Dni == dni);
+        }
+    }
+}
---
+++ cantinaPadel/DAL/Repositories/IClienteRepository.cs
@@ -0,0 +1,17 @@
+﻿using cantinaPadel.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cantinaPadel.DAL.Repositories
+{
+    public interface IClienteRepository
+    {
+        List<Cliente> ObtenerTodos();
+         Cliente? ObtenerPorId(int id);
+         void Agregar(Cliente cliente);
+         void Modificar(Cliente cliente);
+         void Bajalogica(int id);
+        Persona? BuscarPersonaPorDni(string dni);
+    }
+}
